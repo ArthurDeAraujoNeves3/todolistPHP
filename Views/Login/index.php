1c9725325da284564ca0f41ab509bb16d39a69fb
ArthurDeAraujoNeves3 <arthurdearaujoneves333@gmail.com>
2024-08-28 16:07:50 -0300

    Front da página Criar conta / Mais algumas configurações na estruturado projeto

--- Views/Login/index.php
+++ Views/Login/index.php
@@ -2,52 +2,85 @@
 <html lang="pt-br">
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Login</title>
-
-	<!-- FAVICON -->
-	<link rel="shortcut icon" href="<?= BASE_URL; ?>Assets/img/favicon.png" />
-	<link rel="icon" href="<?= BASE_URL; ?>Assets/img/favicon.png" type="image/x-icon" />
-
-	<!-- BOOTSTRAP -->
-	<link rel="stylesheet" href="<?= BASE_URL; ?>Assets/css/bootstrap.min.css">
-
-	<!-- BOOTSTRAP SIGNIN -->
-	<link rel="stylesheet" href="<?= BASE_URL; ?>Assets/css/signin.css">
-
-	<!-- CUSTOM CSS -->
-	<?php if (isset($viewData['CSS'])) {
-		echo $viewData['CSS'];
-	}; ?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo BASE_URL . '/Public/Bootstrap/bootstrap.min.css' ?>">
+	<link rel="stylesheet" href="<?php echo BASE_URL . '/Public/index.css' ?>">
+	<link rel="stylesheet" href="<?php echo BASE_URL . '/Public/styles/LoginStyle.css' ?>">
+	<title>Login | todoist</title>
 </head>
 
-<body class="text-center">
-	<main class="form-signin">
-		<form method="post">
-			<!-- <img class="mb-1 img-fluid" src="<?= BASE_URL.'Assets/img/login.png';?>" alt="IMG"> -->
+<body class="bgLight">
 
-			<div class="form-floating">
-				<input type="email" name="email" class="form-control" id="floatingInput" placeholder="nome@email.com" <?= (isset($viewData["email"]) && !empty($viewData["email"])) ? "value='" . $viewData["email"] . "'" : ""; ?>>
-				<label for="floatingInput">Endereço de Email</label>
-			</div>
-			<div class="form-floating">
-				<input type="password" name="passwd" class="form-control" id="floatingPassword" placeholder="Password">
-				<label for="floatingPassword">Senha</label>
+	<header class="container">
+
+		<nav class="navbar bg-body-tertiary">
+
+			<div class="container">
+
+				<figure>
+
+					<img class="Logo" src="<?php echo BASE_URL . '/Public/assets/img/Logo/TodoistName.png' ?>" alt="Bootstrap" />
+
+				</figure>
+
 			</div>
 
-			<div class="checkbox mb-3">
-				<label>
-					<a href="<?= BASE_URL.'Login/forgoutPassword';?>" class="link-secondary">Esqueci a senha</a>
-				</label>
-			</div>
-			<button class="w-100 btn btn-lg btn-success mb-3" type="submit">Entrar</button>
-			<?php if (isset($viewData["alert"]) && !empty($viewData["alert"])) {
-				echo $viewData["alert"];
-			} ;?>
-			<p class="mt-5 mb-3 text-muted">Desenvolvimento &copy; 2021</p>
-		</form>
+		</nav>
+
+	</header>
+	<main class="container d-flex align-items-center justify-content-between mt-5">
+
+		<section class="d-flex flex-column gap-3 Form">
+
+			<h1>Login</h1>
+
+			<action class="d-flex flex-column">
+				
+				<form method="post" action="<?php echo $_SERVER["PHP_SELF"]?>">
+
+					<section class="d-flex flex-column">
+
+						<div class="input-group mb-3">
+
+							<input name="name" id="name" maxlength="128" type="text" class="form-control" placeholder="Seu nome" aria-label="Email" aria-describedby="basic-addon1">
+
+						</div> <!--Nome do usuário-->
+
+						<div class="input-group mb-3">
+
+							<input name="email" id="email" maxlength="256" type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+
+						</div> <!--Email-->
+
+						<div class="input-group mb-3">
+
+							<input name="password" id="password" maxlength="60" type="password" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1">
+
+						</div> <!--Senha-->
+
+					</section> <!--Inputs-->
+
+					<button type="submit" class="btn btn-primary">Entrar com e-mail</button>
+
+				</form>
+
+			</action> <!--Inputs-->
+
+			<hr>
+
+			<p class="text-center">Já se cadastrou? <a href="<?php echo BASE_URL . "Register" ?>" class="text-decoration-underline">Vá para o login</a></p>
+
+		</section> <!--Form-->
+
+		<section class="Video">
+
+			<video playsinline="" poster="https://todoist.b-cdn.net/assets/images/7b55dafbc1fe203bd537c738fb1757ed.png"><source src="https://todoist.b-cdn.net/assets/video/69a00ecf3b2aedf11010987593926c2e.mp4" type="video/mp4"></video>
+
+		</section> <!--Video-->
+
 	</main>
+
 </body>
 
 </html>
